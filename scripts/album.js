3ff67b6d63a11f396e9b640428ca0031599a39ed
Robert Aguirre <RobertAguirre@users.noreply.github.com>
2017-06-01 19:10:55 -0400

    CP26- Implemented play button

--- scripts/album.js
+++ scripts/album.js
@@ -1,4 +1,3 @@
-
 //Example Album
 var albumPicasso = {
   title: 'The Colors',
@@ -27,43 +26,105 @@ var albumMarconi = {
     { title: 'Ring, ring, ring', duration: '5:01' },
     { title: 'Fits in your pocket', duration: '3:21'},
     { title: 'Can you hear me now?', duration: '3:14' },
-    { title: 'Wrong phone number', duration: '2:15'}
+    { title: 'Wrong phone number', duration: '2:15' }
+  ]
+};
+
+//Third Album Example
+var albumSheeran = {
+  title: 'Divide',
+  artist: 'Ed Sheeran',
+  label: 'Atlanic Reocrds UK',
+  year: '2017',
+  albumArtUrl: 'assets/images/album_covers/Divide_cover.png',
+  songs: [
+    { title: 'Eraser', duration: '3:48' },
+    { title: 'Castle on the Hill', duration: '4:21' },
+    { title: 'Dive', duration: '3:59' },
+    { title: 'Shape of You', duration: '3:54' },
+    { title: 'Perfect', duration: '4:23' },
+    { title: 'Galway Girl', duration: '2:51' },
+    { title: 'Happier', duration: '3:28' },
+    { title: 'New Man', duration: '3:09' },
+    { title: 'Hearts Dont Break Around here', duration: '4:09' },
+    { title: 'What Do I know?', duration: '3:57' },
+    { title: 'How Would You Feel', duration: '4:41' },
+    { title: 'Supermarket Flowers', duration: '3:41' },
+    { title: 'Barcelona', duration: '3:11' },
+    { title: 'Bibia Be Ye Ye', duration: '2:57' },
+    { title: 'Nancy Mulligan', duration: '3:00' },
+    { title: 'Save Myself', duration: '4:07' }
   ]
 };
 
 var createSongRow = function(songNumber, songName, songLength) {
   var template =
     '<tr class="album-view-song-item">'
-    + '<td class="song-item-number">' + songNumber + '</td>'
+    + '<td class="song-item-number" data-song-number = " ' + songNumber + ' ">' + songNumber + '</td>'
     + '<td class="song-item-title">' + songName + '</td>'
     + '<td class="song-item-duration">' + songLength + '</td>'
     ;
+
     return template;
 };
 
-var setCurrentAlbum = function(album) {
-  // #1
-  var albumTitle = document.getElementsByClassName('album-view-title')[0];
-  var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-  var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-  var albumImage = document.getElementsByClassName('album-cover-art')[0];
-  var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+// Global scope elements to populate dynamically
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 
-  // #2
+var setCurrentAlbum = function(album) {
+  // Assugb values to each part of the Album (text, images)
   albumTitle.firstChild.nodeValue = album.title;
   albumArtist.firstChild.nodeValue = album.artist;
   albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
   albumImage.setAttribute('src', album.albumArtUrl);
 
-  // #3
+  // Clear contents of album song list container
   albumSongList.innerHTML = '';
 
-  // #4
+  // Build list of objects from album JavaScript object
   for (var i = 0; i < album.songs.length; i++) {
       albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
   }
 };
 
+//For storing the selected variable in a table to add a listener for a mouseover event
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+var songRows = document.getElementsByClassName('album-view-song-item');
+
+//Album button templates
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+
 window.onload = function() {
   setCurrentAlbum(albumPicasso);
+
+  songListContainer.addEventListener('mouseover', function(event){
+    //Only target individual song rows during event delegation
+    if (event.target.parentElement.className === 'album-view-song-item') {
+      //Change the content from the number to the play button's HTML
+      event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+    }
+  });
+
+  for (var i = 0; i < songRows.length; i++){
+    songRows[i].addEventListener('mouseleave', function(event){
+      //Selects first child element, which is the song-item-number element
+      this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+    });
+  }
+
+/*  var albumsArray = [albumPicasso, albumMarconi, albumSheeran];
+  i = 1;
+
+  albumImage.addEventListener('click', function(x){
+    setCurrentAlbum( albumsArray[i] );
+    i++;
+    if (i == albumsArray.length){
+        i = 0;
+    }
+  })
+*/
 };
