56659228eb29a14353191fbfe81a67f05faf5dd8
Robert Aguirre <RobertAguirre@users.noreply.github.com>
2017-07-10 13:13:43 -0400

    CP32- Next and Previous buttons

--- scripts/album.js
+++ scripts/album.js
@@ -1,62 +1,3 @@
-//Example Album
-var albumPicasso = {
-  title: 'The Colors',
-  artist: 'Pablo Picasso',
-  label: 'Cubism',
-  year: '1881',
-  albumArtUrl: 'assets/images/album_covers/01.png',
-  songs: [
-    {title: 'Blue', duration: '4:26'},
-    {title: 'Green', duration: '3:14'},
-    {title: 'Red', duration: '5:01'},
-    {title: 'Pink', duration: '3:21'},
-    {title: 'Magenta', duration: '2:15'},
-  ]
-};
-
-//Another Example Album
-var albumMarconi = {
-  title: 'The Telephone',
-  artist: 'Guglielmo Marconi',
-  label: 'EM',
-  year: '1909',
-  albumArtUrl: 'assets/images/album_covers/20.png',
-  songs: [
-    { title: 'Hello, Operator?', duration: '1:01' },
-    { title: 'Ring, ring, ring', duration: '5:01' },
-    { title: 'Fits in your pocket', duration: '3:21'},
-    { title: 'Can you hear me now?', duration: '3:14' },
-    { title: 'Wrong phone number', duration: '2:15' }
-  ]
-};
-
-//Third Album Example
-var albumSheeran = {
-  title: 'Divide',
-  artist: 'Ed Sheeran',
-  label: 'Atlanic Reocrds UK',
-  year: '2017',
-  albumArtUrl: 'assets/images/album_covers/Divide_cover.png',
-  songs: [
-    { title: 'Eraser', duration: '3:48' },
-    { title: 'Castle on the Hill', duration: '4:21' },
-    { title: 'Dive', duration: '3:59' },
-    { title: 'Shape of You', duration: '3:54' },
-    { title: 'Perfect', duration: '4:23' },
-    { title: 'Galway Girl', duration: '2:51' },
-    { title: 'Happier', duration: '3:28' },
-    { title: 'New Man', duration: '3:09' },
-    { title: 'Hearts Dont Break Around here', duration: '4:09' },
-    { title: 'What Do I know?', duration: '3:57' },
-    { title: 'How Would You Feel', duration: '4:41' },
-    { title: 'Supermarket Flowers', duration: '3:41' },
-    { title: 'Barcelona', duration: '3:11' },
-    { title: 'Bibia Be Ye Ye', duration: '2:57' },
-    { title: 'Nancy Mulligan', duration: '3:00' },
-    { title: 'Save Myself', duration: '4:07' }
-  ]
-};
-
 var createSongRow = function(songNumber, songName, songLength) {
   var template =
     '<tr class="album-view-song-item">'
@@ -68,39 +9,44 @@ var createSongRow = function(songNumber, songName, songLength) {
     var $row = $(template);
 
     var clickHandler = function() {
-      var songNumber = $(this).attr('data-song-number');
+      var songNumber = parseInt($(this).attr('data-song-number'));
+      console.log("songNumber type is " + typeof songNumber + "\n and currentlyPlayingSongNumber type is " + typeof currentlyPlayingSongNumber);
 
-      if (currentlyPlayingSong !== null){
+      if (currentlyPlayingSongNumber !== null){
         //Revert to song number from currently playing song because user started playing new song.
-        var currentlyPlayingCell = $('.song-item-number[data-song-number="' + currentlyPlayingSong + '"]');
-        currentlyPlayingCell.html(currentlyPlayingSong);
+        var currentlyPlayingCell = $('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]');
+        currentlyPlayingCell.html(currentlyPlayingSongNumber);
       }
 
-      if (currentlyPlayingSong !== songNumber){
+      if (currentlyPlayingSongNumber !== songNumber) {
         //Switch from play to Pause button to indicate new song is playing.
-        $(this).html(pauseButtonTemplate);
-        currentlyPlayingSong = songNumber;
-      } else if (currentlyPlayingSong === songNumber){
+          $(this).html(pauseButtonTemplate);
+          currentlyPlayingSongNumber = songNumber;
+          currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+          updatePlayerBarSong();
+      } else if (currentlyPlayingSongNumber === songNumber) {
         //Switch from Pause to Play button to pause currently playing song.
         $(this).html(playButtonTemplate);
-        currentlyPlayingSong = null;
+        $('.main-controls .play-pause').html(playerBarPlayButton);
+        currentlyPlayingSongNumber = null;
+        currentSongFromAlbum = null;
       }
     };
 
     var onHover = function(event) {
       var songNumberCell = $(this).find('.song-item-number');
-      var songNumber = songNumberCell.attr('data-song-number');
+      var songNumber = parseInt(songNumberCell.attr('data-song-number'));
 
-      if (songNumber !== currentlyPlayingSong) {
+      if (songNumber !== currentlyPlayingSongNumber) {
         songNumberCell.html(playButtonTemplate);
       }
     };
 
     var offHover = function(event) {
       var songNumberCell = $(this).find('.song-item-number');
-      var songNumber = songNumberCell.attr('data-song-number');
+      var songNumber = parseInt(songNumberCell.attr('data-song-number'));
 
-      if (songNumber !== currentlyPlayingSong){
+      if (songNumber !== currentlyPlayingSongNumber){
         songNumberCell.html(songNumber);
       }
     };
@@ -125,6 +71,7 @@ var $albumImage = $('.album-cover-art');
 var $albumSongList = $('.album-view-song-list');
 
 var setCurrentAlbum = function(album) {
+  currentAlbum = album;
   $albumTitle.text(album.title);
   $albumArtist.text(album.artist);
   $albumReleaseInfo.text(album.year + ' ' + album.label);
@@ -138,20 +85,88 @@ var setCurrentAlbum = function(album) {
   }
 };
 
+var trackIndex = function(album, song) {
+    return album.songs.indexOf(song);
+};
+
+var nextSong = function() {
+    var currentSongIndex = trackIndex(currentAlbum, currentSongFromAlbum);
+    // Note that we're _incrementing_ the song here
+    currentSongIndex++;
+
+    if (currentSongIndex >= currentAlbum.songs.length) {
+        currentSongIndex = 0;
+    }
+
+    // Save the last song number before changing it
+    var lastSongNumber = currentlyPlayingSongNumber;
+
+    // Set a new current song
+    currentlyPlayingSongNumber = currentSongIndex + 1;
+    currentSongFromAlbum = currentAlbum.songs[currentSongIndex];
+
+    // Update the Player Bar information
+    updatePlayerBarSong();
+
+    var $nextSongNumberCell = $('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]');
+    var $lastSongNumberCell = $('.song-item-number[data-song-number="' + lastSongNumber + '"]');
+
+    $nextSongNumberCell.html(pauseButtonTemplate);
+    $lastSongNumberCell.html(lastSongNumber);
+};
+
+var previousSong = function() {
+    var currentSongIndex = trackIndex(currentAlbum, currentSongFromAlbum);
+    // Note that we're _decrementing_ the index here
+    currentSongIndex--;
+
+    if (currentSongIndex < 0) {
+        currentSongIndex = currentAlbum.songs.length - 1;
+    }
+
+    // Save the last song number before changing it
+    var lastSongNumber = currentlyPlayingSongNumber;
+
+    // Set a new current song
+    currentlyPlayingSongNumber = currentSongIndex + 1;
+    currentSongFromAlbum = currentAlbum.songs[currentSongIndex];
+
+    // Update the Player Bar information
+    updatePlayerBarSong();
+
+    $('.main-controls .play-pause').html(playerBarPauseButton);
+
+    var $previousSongNumberCell = $('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]');
+    var $lastSongNumberCell = $('.song-item-number[data-song-number="' + lastSongNumber + '"]');
+
+    $previousSongNumberCell.html(pauseButtonTemplate);
+    $lastSongNumberCell.html(lastSongNumber);
+};
+
+var updatePlayerBarSong = function() {
+    $('.currently-playing .song-name').text(currentSongFromAlbum.title);
+    $('.currently-playing .artist-name').text(currentAlbum.artist);
+    $('.currently-playing .artist-song-mobile').text(currentSongFromAlbum.title + " - " + currentAlbum.artist);
+
+    $('.main-controls .play-pause').html(playerBarPauseButton);
+};
+
 //Album button templates
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
+var playerBarPlayButton = '<span class="ion-play"></span>';
+var playerBarPauseButton = '<span class="ion-pause"></span>';
 
 //Store state of playing songs
-var currentlyPlayingSong = null;
+var currentAlbum = null;
+var currentlyPlayingSongNumber = null;
+var currentSongFromAlbum = null;
+
+var $previousButton = $('.main-controls .previous');
+var $nextButton = $('.main-controls .next');
 
 $(document).ready(function(){
   setCurrentAlbum(albumPicasso);
+  $previousButton.click(previousSong);
+  $nextButton.click(nextSong);
 });
-
-//     songRows[i].addEventListener('click', function(event){
-//       clickHandler(event.target);
-//     });
-//   }
-//
-// };
---
+++ scripts/fixtures.js
@@ -0,0 +1,56 @@
+//Example Data Albums
+var albumPicasso = {
+  title: 'The Colors',
+  artist: 'Pablo Picasso',
+  label: 'Cubism',
+  year: '1881',
+  albumArtUrl: 'assets/images/album_covers/01.png',
+  songs: [
+    {title: 'Blue', duration: '4:26'},
+    {title: 'Green', duration: '3:14'},
+    {title: 'Red', duration: '5:01'},
+    {title: 'Pink', duration: '3:21'},
+    {title: 'Magenta', duration: '2:15'},
+  ]
+};
+
+var albumMarconi = {
+  title: 'The Telephone',
+  artist: 'Guglielmo Marconi',
+  label: 'EM',
+  year: '1909',
+  albumArtUrl: 'assets/images/album_covers/20.png',
+  songs: [
+    { title: 'Hello, Operator?', duration: '1:01' },
+    { title: 'Ring, ring, ring', duration: '5:01' },
+    { title: 'Fits in your pocket', duration: '3:21'},
+    { title: 'Can you hear me now?', duration: '3:14' },
+    { title: 'Wrong phone number', duration: '2:15' }
+  ]
+};
+
+var albumSheeran = {
+  title: 'Divide',
+  artist: 'Ed Sheeran',
+  label: 'Atlanic Reocrds UK',
+  year: '2017',
+  albumArtUrl: 'assets/images/album_covers/Divide_cover.png',
+  songs: [
+    { title: 'Eraser', duration: '3:48' },
+    { title: 'Castle on the Hill', duration: '4:21' },
+    { title: 'Dive', duration: '3:59' },
+    { title: 'Shape of You', duration: '3:54' },
+    { title: 'Perfect', duration: '4:23' },
+    { title: 'Galway Girl', duration: '2:51' },
+    { title: 'Happier', duration: '3:28' },
+    { title: 'New Man', duration: '3:09' },
+    { title: 'Hearts Dont Break Around here', duration: '4:09' },
+    { title: 'What Do I know?', duration: '3:57' },
+    { title: 'How Would You Feel', duration: '4:41' },
+    { title: 'Supermarket Flowers', duration: '3:41' },
+    { title: 'Barcelona', duration: '3:11' },
+    { title: 'Bibia Be Ye Ye', duration: '2:57' },
+    { title: 'Nancy Mulligan', duration: '3:00' },
+    { title: 'Save Myself', duration: '4:07' }
+  ]
+};
